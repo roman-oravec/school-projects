59c676c2005d52ca67c8b8955ed96991cdd0b77f
Roman Oravec <xoravec@fi.muni.cz>
2019-06-06 13:54:46 +0300

    added 2 projects

---
+++ spring boot microservices/movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,38 @@
+package com.example.moviecatalogservice.resources;
+
+
+import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+            // For each, call movie info service and get info about movie
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            // Put it together
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+    })
+                .collect(Collectors.toList());
+
+
+    }
+}
